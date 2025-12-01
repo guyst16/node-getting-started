0324b1e8e5f809dd02529318c37014d2859ccfd2
Guy Steinberger <guy.s@getport.io>
2025-12-01 12:32:31 +0200

    Replace kubectl apply command in okteto.yml with envsubst to substitute environment variables in k8s.yml during deployment

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = 3000;
 
 app.get('/', function (req, res) {
-  res.send('Hello world! NodeJS App 2 test preview environment'); 
+  res.send('Hello world! NodeJS App 2 test preview environment 2'); 
 })
 
 app.listen(PORT, function () {
